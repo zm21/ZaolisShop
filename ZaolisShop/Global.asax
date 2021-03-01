83fd88b5ecf7eaa0f8d0a28d6564203e64d9f1d9
zm21 <zaietsmikola.21@gmail.com>
2021-03-01 17:55:30 +0200

    Add project files.

---
+++ ZaolisShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZaolisShop.MvcApplication" Language="C#" %>
---
+++ ZaolisShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZaolisShop.Startup))]
+namespace ZaolisShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
