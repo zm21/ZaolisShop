d047e2416bb82168ff2596ef0621c8681df5f017
redfizandi@gmail.com <redfizandi@gmail.com>
2021-03-02 21:29:48 +0200

    Added DAL

---
+++ DAL/Entities/Cart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public virtual ICollection<Product> Products { get; set; }
+
+        [Required]
+        public string UserAdditionalInfoId { get; set; }
+
+        [Required]
+        public virtual UserAdditionalInfo UserAdditionalInfo { get; set; }
+    }
+}
---
+++ ZaolisShop/Helper/SeederDatabase.cs
@@ -0,0 +1,48 @@
+﻿using DAL.EF;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZaolisShop.Helper
+{
+    public class SeederDatabase
+    {
+        public static void SeedData()
+        {
+            var context = new ApplicationDbContext();
+            SeedUsers(context);
+        }
+        private static void SeedUsers(ApplicationDbContext _context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            if (!roleManager.Roles.Any())
+            {
+                IdentityRole roleAdmin = new IdentityRole()
+                {
+                    Name = "Admin",
+                };
+                IdentityRole roleShopper = new IdentityRole()
+                {
+                    Name = "Shopper",
+                };
+                roleManager.Create(roleAdmin);
+                roleManager.Create(roleShopper);
+            }
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(_context));
+            if (!userManager.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                };
+
+                userManager.Create(user, "Qwerty+1");
+                userManager.AddToRole(user.Id, "Admin");
+            }
+        }
+    }
+}
