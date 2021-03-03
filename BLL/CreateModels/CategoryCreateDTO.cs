ce93de60b906dc778e55172477c2f87b131bef53
redfizandi@gmail.com <redfizandi@gmail.com>
2021-03-03 21:17:54 +0200

    Added Create DTO'S

---
+++ BLL/CreateModels/CategoryCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.CreateModels
+{
+    class CategoryCreateDTO
+    {
+        public string Name { get; set; }
+    }
+}
