4c3c2cda81d19a33357fb4f2788a09695375b73d
zm21 <zaietsmikola.21@gmail.com>
2021-03-03 22:21:36 +0200

    admin area

---
+++ ZaolisShop/Areas/Admin/Controllers/AdminPanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZaolisShop.Areas.Admin.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        // GET: Admin/AdminPanel
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
