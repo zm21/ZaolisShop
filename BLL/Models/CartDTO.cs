673fdd1f92a8e065fd28d097f41d9d29142c6f06
redfizandi@gmail.com <redfizandi@gmail.com>
2021-03-03 18:43:07 +0200

    Working with DTO

---
+++ BLL/Models/CartDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DAL.Entities
+{
+    [DataContract]
+    public class Cart
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public virtual ICollection<Product> Products { get; set; }
+
+        [DataMember]
+        public string UserAdditionalInfoId { get; set; }
+
+    }
+}
---
+++ BLL/Models/OrderDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Order
+    {
+        public Order()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserAdditionalInfoId { get; set; }
+
+        [Required]
+        public int ShippingAdressId { get; set; }
+
+        [Required]
+        public string DateOfOrder { get; set; }
+
+        [Required]
+        public virtual ICollection<Product> Products { get; set; }
+
+        [Required]
+        public virtual ShippingAdress ShippingAdress { get; set; }
+
+        [Required]
+        public virtual UserAdditionalInfo UserAdditionalInfo { get; set; }
+    }
+}
