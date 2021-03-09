c9683716ff755f9fb896fa560afc05a790d39d73
redfizandi@gmail.com <redfizandi@gmail.com>
2021-03-09 21:46:03 +0200

    Some fixes

---
+++ ZaolisShop/Helper/Payment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Web;
+
+public partial class Payment : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Post back to either sandbox or live
+        string strSandbox = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+        string strLive = "https://www.paypal.com/cgi-bin/webscr";
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(strSandbox);
+
+        //Set values for the request back
+        req.Method = "POST";
+        req.ContentType = "application/x-www-form-urlencoded";
+        byte[] param = Request.BinaryRead(HttpContext.Current.Request.ContentLength);
+        string strRequest = Encoding.ASCII.GetString(param);
+        strRequest += "&cmd=_notify-validate";
+        req.ContentLength = strRequest.Length;
+
+        //Send the request to PayPal and get the response
+        StreamWriter streamOut = new StreamWriter(req.GetRequestStream(), System.Text.Encoding.ASCII);
+        streamOut.Write(strRequest);
+        streamOut.Close();
+        StreamReader streamIn = new StreamReader(req.GetResponse().GetResponseStream());
+        string strResponse = streamIn.ReadToEnd();
+        streamIn.Close();
+
+        if (strResponse == "VERIFIED")
+        {
+            //check the payment_status is Completed
+            //check that txn_id has not been previously processed
+            //check that receiver_email is your Primary PayPal email
+            //check that payment_amount/payment_currency are correct
+            //process payment
+        }
+        else if (strResponse == "INVALID")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
