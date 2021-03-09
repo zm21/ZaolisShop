03e63c8fd82f32d03000017523769c9e62508b99
zm21 <zaietsmikola.21@gmail.com>
2021-03-09 18:43:25 +0200

    some fix

--- ZaolisShop/Areas/Admin/Controllers/AdminPanelController.cs
+++ ZaolisShop/Areas/Admin/Controllers/AdminPanelController.cs
@@ -3,12 +3,12 @@ using DAL.EF;
 using DAL.Entities;
 using DAL.Interfaces;
 using DAL.Repositories;
-using DTO.CreateModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL.CreateModels;
 
 namespace ZaolisShop.Areas.Admin.Controllers
 {
