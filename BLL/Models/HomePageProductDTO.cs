2a7d1e02b9e3da8684c3893a55d87e5f121243c6
zm21 <zaietsmikola.21@gmail.com>
2021-03-11 19:04:22 +0200

    ProductInfDTO

---
+++ BLL/Models/HomePageProductDTO.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class HomePageProductDTO
+    {
+        public List<ProdutcInfDTO> ProductsNew { get; set; }
+
+        public List<ProdutcInfDTO> ProductsPopular { get; set; }
+        public HomePageProductDTO()
+        {
+            ProductsNew = new List<ProdutcInfDTO>();
+            ProductsPopular = new List<ProdutcInfDTO>();
+        }
+    }
+}
---
+++ BLL/Models/ProdutcInfDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace BLL.Models
+{
+    public class ProductInfDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Img1 { get; set; }
+
+        public string Img2 { get; set; }
+    }
+}
