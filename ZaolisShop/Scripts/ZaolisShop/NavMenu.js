211533440e22b4347a9ec8e7cf76d9174c43b752
Богдан Исламов <bodyanice.islamov@gmail.com>
2021-03-11 20:23:50 +0200

    navbarMenu

--- ZaolisShop/Scripts/ZaolisShop/NavMenu.js
+++ ZaolisShop/Scripts/ZaolisShop/NavMenu.js
@@ -7,3 +7,16 @@
             $('.dropdown-menu', this).stop().fadeOut("fast");
         });
 });
+
+
+
+window.addEventListener('scroll', function () {
+    if (document.documentElement.getBoundingClientRect().top == 0) {
+        $('#top-nav-index').css('background-color', 'rgba(0,0,0,0)');
+        $('#top-nav-index').css('border-bottom', '0.5px solid rgb(199, 199, 199)');
+    }
+    else {
+        $('#top-nav-index').css('background-color', 'black');
+        $('#top-nav-index').css('border-bottom', '0px solid black');
+    }
+});
